b8c7734cc3b5def41e18c636169c6438ba909ba0
alessandrofuda <alessandro.fuda@gmail.com>
2017-09-05 09:35:19 +0000

    footer pages views updated

--- en/wexsite/resources/views/front/privacy-policy.php
+++ en/wexsite/resources/views/front/privacy-policy.php
@@ -13,40 +13,61 @@
 							</div>
 							<div class="column one column_column">
 								<div class="column_attr ">
-<p><strong>PRIVACY POLICY PURSUANT TO ART. 13 OF THE ITALIAN LEGIS. DECREE 196/2003</strong><br>
-  <strong>related to the website </strong><a href="https://www.wexplore.co"><strong>www.wexplore.co</strong></a><strong> </strong><br><br>
-  The company ADiValue – Gielle Sas, owner of the website <a href="http://www.wexplore.co">www.wexplore.co</a> and of its related services, is the owner of the treatment of your personal data, pursuant to Art. 13 of the Italian Legis. Decree (herewith Privacy Code). As such, the company wishes to inform you that the data provided upon registration and during the subsequent use of the purchased services, will be treated in a fair, rightful, and transparent way, as detailed below. </p>
-<p><strong>1. Purpose of the treatment<br />
-</strong>Your personal information (including but not limited to name, surname, gender, date of birth, home address, e-mail address, phone number, education, career history, and all other information contained in your resume or CV) provided upon Registration on the Website will be treated with the following purposes:</p>
-<p><strong>1.1 Management of contract, administrative, and tax duties.<br />
-</strong>All the information provided are collected and managed by the Owner and by third companies duly appointed to the management of such data for the purpose of complying to and managing all obligations deriving from the contract related to the Services offered, including tax duties and Company administration, as indicated in art. 5 of the General Conditions. The same article specifies how the users may make use of the services, as well as the contents of the Payment Services that can be purchased by registered Users. <br />
-  In addition, the data provided will be communicated to commercial partner companies of Gielle to ensure the proper delivery of the purchased services, including relocation companies, law firms, outplacement companies or language schools. Specifically, these subjects will be allowed to interact with the User to conduct consultancy online sessions and to exchange the necessary documentation for the delivery of said services. <br />
-  The owner commits to make use of the received information in compliance to the applicable norms and legislation, specifically those concerning the privacy of personal data.</p>
-<p><strong>1.2 Marketing activities.<br />
-</strong>Only upon your free and explicit consent, your personal data may be treated:</p>
+
+
+
+
+
+<p>
+<b>PRIVACY POLICY PURSUANT TO ART. 13 OF THE ITALIAN LEGIS. DECREE 196/2003<br>
+related to the website <a href="https://www.wexplore.co"><strong>www.wexplore.co</strong></a> </b><br><br>
+
+The company Wexplore Srl, owner of the website <a href="https://www.wexplore.co"><strong>www.wexplore.co</strong></a> and of its related services, is the owner of the treatment of your personal data, pursuant to Art. 13 of the Italian Legis. Decree (herewith Privacy Code). As such, the company wishes to inform you that the data provided upon registration and during the subsequent use of the purchased services, will be treated in a fair, rightful, and transparent way, as detailed below.<br><br>
+
+<b>1. Purpose of the treatment</b><br>
+Your personal information (including but not limited to name, surname, gender, date of birth, home address, e-mail address, phone number, education, career history, and all other information contained in your resume or CV) provided upon Registration on the Website will be treated with the following purposes:<br><br>
+
+<b>1.1 Management of contract, administrative, and tax duties.</b><br>
+All the information provided are collected and managed by the Owner and by third companies duly appointed to the management of such data for the purpose of complying to and managing all obligations deriving from the contract related to the Services offered, including tax duties and Company administration, as indicated in art. 5 of the General Conditions. The same article specifies how the users may make use of the services, as well as the contents of the Payment Services that can be purchased by registered Users. <br>
+In addition, the data provided will be communicated to commercial partner companies of Wexplore to ensure the proper delivery of the purchased services, including relocation companies, law firms, outplacement companies or language schools. Specifically, these subjects will be allowed to interact with the User to conduct consultancy online sessions and to exchange the necessary documentation for the delivery of said services. <br>
+The owner commits to make use of the received information in compliance to the applicable norms and legislation, specifically those concerning the privacy of personal data.<br><br>
+
+<b>1.2 Marketing activities.</b><br>
+Your personal data may be treated:<br>
 <ul>
-  <li>For marketing purposes, sending of advertising material, sale through automated devices (such as fax, sms, e-mail), sending of commercial communications both interactive and traditional such as via mail, telephone and/or any other technique of long-distance communication, related to the product or services offered by our Company, as well as to allow said Company to conduct market researches;</li>
-  <li>To allow the Company to conduct statistical analysis in order to improve its products and services.</li>
+	<li>For communication purposes of communication and update on company news, developments, and initiatives , sending of institutional communications both interactive and traditional such as via mail, telephone and/or any other technique of long-distance communication, related to the product or services offered by our Company, as well as to allow said Company to conduct market researches;</li>
+	<li>To allow the Company to conduct statistical analysis in order to improve its products and services.</li>
 </ul>
-<p><strong>1.3 Communication of anonymous data for statistical purposes. <br />
-</strong>Only upon your non mandatory and freely expressed consent to the anonymization of the transferred data, said data may be communicated exclusively in an aggregated forms to associations or newspapers and magazines.<br />
-  The aggregated data will not allow in any way whatsoever to identify the single user (for instance, one such data may be the calculation of the percentage of individuals living in a given geographical area who choose to relocate), and will have a mere statistical and totally anonymous purpose to help map the phenomenon of the migration of professionals abroad. Said data may be communicated exclusively in an aggregated forms to associations or newspapers and magazines.</p>
-<p><strong>2. Treatment of the conferred data.<br />
-</strong>The treatment of personal data will be done exclusively through digital support. The owner warrants the compliance to the minimum safety standards provided for by the Code in terms of protection of personal data, aimed to prevent any loss or alteration of the data, as well as the diffusion or improper access to said data.</p>
-<p><strong>3. Discretionary nature of the conferring of data and consequences of the refusal to data treatment<br />
-</strong>The conferring of the data is entirely discretional. <strong></strong><br />
-  The denial of the consent to the treatment of the data for the purposes stated in Art. 1.1. will make it impossible to enjoy the service provided by the Owner. <strong></strong><br />
-  The denial of the consent to the treatment of the data for the purposes stated in Art. 1.2 will not prejudice the contractual relationship; however it will make it impossible to use the data provided for statistical purposes related to the phenomenon of highly skilled migrants.  </p>
-<p><strong>4. Parties to which User&rsquo;s personal data can be communicated and the extent to which said data can be shared. <br />
-</strong>The data provided by the user (such as name, surname, email, and nationality) will be communicated to Gielle&rsquo;s partners as a necessary measure to ensure the correct delivery of the services purchased by the User. Said partners (such as itim International Oy, based in Finland) will be appointed as joint data owners. <br />
-  For further information on the how the User can request an updated list of the appointed owners for the treatment of personal data, please refer to Art. 6 below. </p>
-<p><strong>5. User&rsquo;s rights.<br />
-</strong>In any moment the User may exercise his/her rights towards the Owner pursuant to art. 7 of the Italian Legis. Decree 196 of June 30th, 2003; that is, the User has the right to request in any moment a confirmation of the existence of the provided data, to know their content and origin, to verify their correctness and ask for their integration, update, or rectification (pursuant to art. 7 of the Code). Pursuant to the same article, the User has the right to ask for the data to be cancelled, converted in anonymous form, or object to their treatment for any legitimate reason, including the violations of the applicable laws.</p>
-<p><strong>6. Appointed Owners for the treatment.<br />
-</strong>The User may obtain an updated list of the appointed owners for the treatment of users&rsquo; data by sending such a request to the following email address: <a href="mailto:info@wexplore.co">info@wexplore.co</a>, stating as object of the message: &ldquo;Request appointed owners&rsquo; list&rdquo;. <br />
-  <br />
+Only upon your non mandatory and freely expressed consent, your personal data may be trated for marketing purposes, namely for sending of advertising material and sale through automated tools (such as fax, text message, e-mail), sending of commercial communications, both interactive and traditional, such as mail and phone, or any other long-distance communication technique, related to the products or services offered by our Company.<br>
+In any moment you will have the possibility to unsubscribe from the newsletter, or to otherwise indicate your unwillingness to be contacted through the mediums mentioned above. <br><br>
+ 
+<b>1.3 Communication of anonymous data for statistical purposes.</b> <br>
+Your data may be also communicated, exclusively in an aggregated form, to associations or newspapers and magazines.<br>
+The aggregated data will not allow in any way whatsoever to identify the single user (for instance, one such data may be the calculation of the percentage of individuals living in a given geographical area who choose to relocate), and will have a mere statistical and totally anonymous purpose to help map the phenomenon of the migration of professionals abroad. Said data may be communicated exclusively in an aggregated form to associations or newspapers and magazines.<br><br>
+
+<b>2. Treatment of the conferred data.</b><br>
+The treatment of personal data will be done exclusively through digital support. The owner warrants the compliance to the minimum safety standards provided for by the Code in terms of protection of personal data, aimed to prevent any loss or alteration of the data, as well as the diffusion or improper access to said data.<br><br>
+
+<b>3. Discretionary nature of the conferring of data and consequences of the refusal to data treatment</b><br>
+The conferring of the data is entirely discretional. <br>
+The denial of the consent to the treatment of the data for the purposes stated in Art. 1.1. will make it impossible to enjoy the service provided by the Owner. <br>
+The denial of the consent to the treatment of the data for the purposes stated in Art. 1.2 will not prejudice the contractual relationship; however it will make it impossible to use the data provided for statistical purposes related to the phenomenon of highly skilled migrants.  <br><br>
+
+<b>4. Parties to which User’s personal data can be communicated and the extent to which said data can be shared. </b>
+The data provided by the user (such as name, surname, email, and nationality) will be communicated to Wexplore’s partners as a necessary measure to ensure the correct delivery of the services purchased by the User. Said partners (such as itim International Oy, based in Finland) will be appointed as joint data owners. <br>
+For further information on the how the User can request an updated list of the appointed owners for the treatment of personal data, please refer to Art. 6 below.<br><br>
+
+<b>5. User’s rights.</b><br>
+In any moment the User may exercise his/her rights towards the Owner pursuant to art. 7 of the Italian Legis. Decree 196 of June 30th, 2003; that is, the User has the right to request in any moment a confirmation of the existence of the provided data, to know their content and origin, to verify their correctness and ask for their integration, update, or rectification (pursuant to art. 7 of the Code). Pursuant to the same article, the User has the right to ask for the data to be cancelled, converted in anonymous form, or object to their treatment for any legitimate reason, including the violations of the applicable laws.<br><br>
+
+<b>6. Appointed Owners for the treatment.</b><br>
+The User may obtain an updated list of the appointed owners for the treatment of users’ data by sending such a request to the following email address: <a href="mailto:info@wexplore.co">info@wexplore.co</a>, stating as object of the message: “Request appointed owners’ list”.<br><br>
+
+
 </p>
-</div>
+
+
+								</div>
 							</div>
 
 							</div>
