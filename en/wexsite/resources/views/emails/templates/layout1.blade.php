af2949ff1d3e3108601bebd1c838b865afe7de14
alessandrofuda <alessandro.fuda@gmail.com>
2018-09-10 10:05:16 +0000

    fix images on notification emails

--- en/wexsite/resources/views/emails/templates/layout1.blade.php
+++ en/wexsite/resources/views/emails/templates/layout1.blade.php
@@ -117,7 +117,7 @@
 																										<tr>
 																											<td valign="top" align="center" style="border-collapse:collapse;font-size:0px;line-height:0px;">
 																												<a style="display:inline-block;font-size:0px;" href="{{ url('/') }}" target="_blank" title="Go to Wexplore Site">
-																													<img width="540" border="0" style="outline-style:none;text-decoration:none;border:none;font-size:12px;line-height:16px;margin:0;" src="https://c.emailsys1a.net/mailingassets/824709be2349f301df65dbefc87532459d5b35de.png" alt="" />
+																													<img width="540" border="0" style="outline-style:none;text-decoration:none;border:none;font-size:12px;line-height:16px;margin:0;" src="https://wexplore.co/en/frontend/images/emails/header_mail.png" alt="" />
 																												</a>
 																											</td>
 																										</tr>
@@ -241,7 +241,7 @@
 																										<tr>
 																											<td valign="top" align="center" style="border-collapse:collapse;font-size:0px;line-height:0px;">
 																												<a style="display:inline-block;font-size:0px;" href="{{ url('/') }}" target="_blank" title="Go to Wexplore Site">
-																													<img width="25" border="0" style="max-width:25px;outline-style:none;text-decoration:none;border:none;font-size:12px;line-height:16px;margin:0;" src="https://c.emailsys1a.net/mailingassets/64f68c40ee4996891dcf9d243fedb08d5919276b.png" alt="" />
+																													<img width="25" border="0" style="max-width:25px;outline-style:none;text-decoration:none;border:none;font-size:12px;line-height:16px;margin:0;" src="https://wexplore.co/en/frontend/images/emails/logo_wexplore_email.png" alt="" />
 																												</a>
 																											</td>
 																										</tr>
