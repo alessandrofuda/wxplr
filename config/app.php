60b478e87b79c4e423801caa3caef6c4928f3157
alessandro <alessandro.fuda@gmail.com>
2019-03-29 18:00:05 +0000

    upgrade to L5.4.*

--- config/app.php
+++ config/app.php
@@ -136,7 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-		Collective\Html\HtmlServiceProvider::class,
+		// Collective\Html\HtmlServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -198,8 +198,8 @@ return [
         'URL'        => Illuminate\Support\Facades\URL::class,
         'Validator'  => Illuminate\Support\Facades\Validator::class,
         'View'       => Illuminate\Support\Facades\View::class,
-		'Form' 		 => Collective\Html\FormFacade::class,
-		'Html' 		 => Collective\Html\HtmlFacade::class,
+		// 'Form' 		 => Collective\Html\FormFacade::class,
+		// 'Html' 		 => Collective\Html\HtmlFacade::class,
 		'PDF'        => Barryvdh\DomPDF\Facade::class,
 		'Vimeo' 	 => Vinkla\Vimeo\Facades\Vimeo::class,
 		'Excel'      => Maatwebsite\Excel\Facades\Excel::class,
