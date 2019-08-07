2a4823e9dd3e5adc7547bc1c57a60a12e0022515
alessandro <alessandro.fuda@gmail.com>
2019-08-07 16:28:08 +0000

    chat pro

--- app/Http/Controllers/GotProController.php
+++ app/Http/Controllers/GotProController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -21,7 +20,7 @@ class GotProController extends Controller {
         if($this->paymentCheck($this->service_id)) {
             $data['payed'] = true;
         }
-        
+
         return view('client.got_pro_intro', $data);
     }
 
