560386443c64370243a1a1c8841d69607d15c32b
alessandro <alessandro.fuda@gmail.com>
2019-08-26 15:07:21 +0000

    got-pro chatbot script update (to fix https)

--- resources/views/client/got_pro.blade.php
+++ resources/views/client/got_pro.blade.php
@@ -2,8 +2,12 @@
 
 @push('js-libraries')
 	<!--got pro chatbot-->
-    <script src="https://www.ew9102bot.it/lib/signalr/signalr.js"></script>
-	<script src="https://www.ew9102bot.it/js/ewbot.js"></script>
+    {{-- 
+    	<script src="https://www.ew9102bot.it/lib/signalr/signalr.js"></script>
+		<script src="https://www.ew9102bot.it/js/ewbot.js"></script> 
+	--}}
+	<script src="http://94.76.213.73:9999/lib/signalr/signalr.js"></script>
+	<script src="http://94.76.213.73:9999/js/ewbot.js"></script>
 @endpush
 
 {{-- @section('content')
@@ -57,22 +61,39 @@
 			obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 		}
 
+		//ricerca id sessione nella query string GET (SOLO PER SVILUPPO)
+		function queryParameter(name){
+			var parameters = window.location.search.substring(1);
+			var properties = parameters.split("&");
+			var result = null;
+			properties.forEach(function(p){
+				var tmp = p.split("=");
+				if(tmp[0] == name){
+					result = tmp[1];
+				}
+			});
+			
+			return result;
+		}
+
 		if(CheckCompatibility()){
 			var chat = new Ewbot();
 			chat.Init({
-				hubendpoint : 'https://www.ew9102bot.it/Ewbot',
+				hubendpoint : 'http://94.76.213.73:9999/Ewbot',  // 'https://www.ew9102bot.it/Ewbot',
 				chat_image_welcome : '{{asset('custom/chat-vic/logo_apri_chat.png')}}',
 				chat_image_header_opened : '{{asset('custom/chat-vic/header_chat.png')}}',
 				chat_image_header_closed : '{{asset('custom/chat-vic/logo_chat_aperta.png')}}',			
 				chat_send : '{{asset('custom/chat-vic/chat_send_msg.png')}}',
 				css_headers : '{{asset('custom/chat-vic/chat_CT.css')}}',
-				flow: 'WeExploreFlowNew',
-				disable_signalR: false,
-				context: 'nn...',
+				flow: 'Wexplore_GOT',  // 'WeExploreFlowNew',
+				disable_signalR: true,  // false,
+				// context: 'nn...',
+				session_id: queryParameter("session_id"),
 				parameters: 
 				{ 
 					user_id: {{Auth::user()->id}},
-					chat_object_name: 'chat'
+					chat_object_name: 'chat',
+					current_page: window.location.href
 				}
 			});
 			chat.InitializeContent();
