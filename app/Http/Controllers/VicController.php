6ead934de21ecd378355a573a36488a471e2f9ec
alessandro <alessandro.fuda@gmail.com>
2019-09-27 14:52:55 +0000

    fixed queries for reports generation

--- app/Http/Controllers/VicController.php
+++ app/Http/Controllers/VicController.php
@@ -72,7 +72,7 @@ class VicController extends Controller {
         return $vic_b2c_current_user_chat->where('IdQuestionResponse', $IdQuestionResponse)->first()->Value ?? null;
     }
 
-    public function fetchReportsData() {
+    public function fetchPreparationReportData() {
         $user_id = Auth::user()->id;
         $vic_b2c_current_user_chat = DB::connection('ewhere')->table('wexpl_vic_b2c_reports')->where('IdUser', $user_id)->orderBy('crdate', 'DESC')->get();
 
@@ -82,8 +82,6 @@ class VicController extends Controller {
         $target_country = $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, 'country') ?? 'n.a.';
 
         $target_country_info = DB::connection('ewhere')->table('Matrice_VIC_B2C')->where('paese', $target_country)->orderBy('Id', 'DESC')->first() ?? null;
-        // $target_country_id = VicB2CMatrix::where('paese', $target_country)->orderBy('Id', 'DESC')->first()->Id ?? null;
-        // $target_country_info = VicB2CMatrix::find($target_country_id); 
         $target_country_name = $target_country_info->paese ?? 'n.a.';
         $main_product_sectors = $target_country_info->Testo2_3_1_5 ?? 'n.a.';
         $your_selection_on_product_sectors = $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '1_6') ?? 'n.a.'; 
@@ -168,39 +166,16 @@ class VicController extends Controller {
             $cover_letter_score = $cover_letter_sum.' su '. count($cover_letter);
         }
 
-        /*job-hunt report2*/
-        $useful_sites_head_hunter = $target_country_info->Testo2_3_7_20 ?? 'n.a.';
-        $useful_sites_job_board = $target_country_info->Testo2_3_7_20 ?? 'n.a.';
-        $useful_sites_networking = $target_country_info->Testo2_3_7_20 ?? 'n.a.';
+        
 
-        $scores = DB::connection('ewhere')->table('vVic_b2c_punti_6_7_8')->where('IdUser', $user_id)->get();
-        $score = 'n.a.';
-        $head_hunter_score = $scores->where('report6_iscompleted', 1)->first()->report6_sum ?? null; // nel DB e-where: è la view vVic_b2c_punti_6_7_8 colonne: report6 
-        $job_board_score = $scores->where('report7_iscompleted', 1)->first()->report7_sum ?? null; // nel DB e-where: è la view vVic_b2c_punti_6_7_8 colonna: report7
-        $network_score = $scores->where('report8_iscompleted', 1)->first()->report8_sum ?? null; // nel DB e-where: è la view vVic_b2c_punti_6_7_8 colonna: report8
-        if($head_hunter_score && $job_board_score && $network_score) {
-            $points = $head_hunter_score + $job_board_score + $network_score;
-            $total_base_score = count($scores);
-            $score = $points.' su '.$total_base_score;
-        } 
-        /*STAR method items*/
-        $star = [
-            's' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_4') ?? 'n.a.',
-            't' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_6') ?? 'n.a.',
-            'a' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_8') ?? 'n.a.',
-            'r' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_10') ?? 'n.a.',
-        ];
-
-        $final_recommendations = $target_country_info->Testo2_3_11_5 ?? '-';
-
-        return compact('target_country_name', 'main_product_sectors', 'your_selection_on_product_sectors', 'geographic_area_where_you_move', 'local_language_knowledge', 'local_language_knowledge_level', 'your_goal', 'your_motivation', 'target_role', 'target_sector', 'modality', 'cultural_fit', 'gaps', 'cv_check', 'cv_check_score', 'cover_letter', 'cover_letter_score', 'useful_sites_head_hunter', 'useful_sites_job_board', 'useful_sites_networking', 'score', 'final_recommendations', 'star');
+        return compact('target_country_name', 'main_product_sectors', 'your_selection_on_product_sectors', 'geographic_area_where_you_move', 'local_language_knowledge', 'local_language_knowledge_level', 'your_goal', 'your_motivation', 'target_role', 'target_sector', 'modality', 'cultural_fit', 'gaps', 'cv_check', 'cv_check_score', 'cover_letter', 'cover_letter_score');
     }
 
 
     public function generatePreparationReport() {
 
-        $data = $this->fetchReportsData();
-       
+        $data = $this->fetchPreparationReportData();
+
         if(!$data) {
             return  back()->with('error', 'You haven\'t compiled Vic yet');
         }
@@ -211,8 +186,6 @@ class VicController extends Controller {
         $data['title'] = 'WELCOME IN WEXPLORE<br/>SBLOCCA IL POTENZIALE DELLA TUA CARRIERA';
         $data['meta_title'] = 'Vic Preparation Report';
 
-        //dd($data);
-
         $pdf = PDF::loadView('reports.vic-b2c-preparation', $data);
         // return view('reports.vic-b2c-preparation', $data);
         // return $pdf->stream(); // load pdf in browser
@@ -221,10 +194,49 @@ class VicController extends Controller {
 
     }
 
+    public function fetchJobHuntReportData() {
+
+        $user_id = Auth::user()->id;
+        $vic_b2c_current_user_chat = DB::connection('ewhere')->table('wexpl_vic_b2c_reports')->where('IdUser', $user_id)->orderBy('crdate', 'DESC')->get();
+
+        if(count($vic_b2c_current_user_chat) == 0 || !$vic_b2c_current_user_chat) {
+            return null;
+        }
+
+        $target_country = $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, 'country') ?? 'n.a.';
+        $target_country_info = DB::connection('ewhere')->table('Matrice_VIC_B2C')->where('paese', $target_country)->orderBy('Id', 'DESC')->first() ?? null;
+        $target_country_name = $target_country_info->paese ?? 'n.a.';
+        
+        $useful_sites_head_hunter = $target_country_info->Testo2_3_7_20 ?? 'n.a.';
+        $useful_sites_job_board = $target_country_info->Testo2_3_7_20 ?? 'n.a.';
+        $useful_sites_networking = $target_country_info->Testo2_3_7_20 ?? 'n.a.';
+
+        $score = 'n.a.';
+        $scores = DB::connection('ewhere')->table('vVic_b2c_punti_6_7_8')->where('IdUser', $user_id)->get(); // !! query sulla view mysql -> collo di bottiglia !!
+        $head_hunter_score = $scores->where('report6_iscompleted', 1)->first()->report6_sum ?? null; // nel DB e-where: è la view vVic_b2c_punti_6_7_8 colonne: report6 
+        $job_board_score = $scores->where('report7_iscompleted', 1)->first()->report7_sum ?? null; // nel DB e-where: è la view vVic_b2c_punti_6_7_8 colonna: report7
+        $network_score = $scores->where('report8_iscompleted', 1)->first()->report8_sum ?? null; // nel DB e-where: è la view vVic_b2c_punti_6_7_8 colonna: report8
+        if($head_hunter_score && $job_board_score && $network_score) {
+            $points = $head_hunter_score + $job_board_score + $network_score;
+            $total_base_score = count($scores);
+            $score = $points.' su '.$total_base_score;
+        } 
+        $star = [
+            's' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_4') ?? 'n.a.',
+            't' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_6') ?? 'n.a.',
+            'a' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_8') ?? 'n.a.',
+            'r' => $this->getResponseFromVicB2CChat($vic_b2c_current_user_chat, '9_10') ?? 'n.a.',
+        ];
+        $final_recommendations = $target_country_info->Testo2_3_11_5 ?? '-';
+
+        return compact('target_country_name', 'useful_sites_head_hunter', 'useful_sites_job_board', 'useful_sites_networking', 'score', 'star', 'final_recommendations');
+
+    }
 
     public function generateJobHuntReport() {
 
-        $data = $this->fetchReportsData();
+        $data = $this->fetchJobHuntReportData();
+        
         if(!$data) {
             return  back()->with('error', 'You haven\'t compiled Vic yet');
         }
@@ -240,19 +252,12 @@ class VicController extends Controller {
         // return $pdf->stream(); // load pdf in browser
 
         return $pdf->download('vic-b2c-job-hunt-report-'.Str::slug($data['full_name'], '-').'-'.date('Y-m-d').'-'.time().'.pdf');
-
     }
-
-
-    
 
 
     public function generateTakeOffReport() {
         return 'work in progress';
     }
-
-
-
 
 
     public function reportDocumentDownload($document_name) {
